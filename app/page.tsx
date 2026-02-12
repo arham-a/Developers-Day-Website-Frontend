a83f80cac0c49936e85fa40d412dfb4bac3746e8
arham-a <arrham.10@gmail.com>
2026-02-12 21:20:37 +0500

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome</h1>
+        <p className="text-lg mt-4">Start building your website here</p>
+      </div>
+    </section>
+  );
+}
